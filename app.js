a6927b04c9d5c11e80f73d44b79daeac6dbc7401
Keftcha <alban.dewilde@gmail.com>
2022-02-25 20:02:44 +0100

    Separate code brick and prepare to show attempts

--- app.js
+++ app.js
@@ -1,29 +1,90 @@
+"use strict"
+
+let guess = ""
+
+const isValidInput = (input) => {
+    return input.length == 64
+}
+
+const unvalidInput = () => {
+    const inputField = document.getElementById("input")
+    inputField.style.borderColor = "red"
+
+    const errMsg = document.getElementById("err")
+    errMsg.innerHTML = "The sha256 must be 64 caracters long"
+}
+
+const validInput = () => {
+    const inputField = document.getElementById("input")
+    inputField.style.borderColor = null
+
+    const errMsg = document.getElementById("err")
+    errMsg.innerHTML = ""
+}
+
+const addTry = (guess) => {
+    console.log(guess)
+}
+
+const addInputField = (parentNode) => {
+    // Text box to input sha256
+    const inputField = document.createElement("input")
+    inputField.type = "text"
+    inputField.id = "input"
+    inputField.maxLength = 64
+    inputField.autofocus = "autofocus"
+    inputField.size = 64
+    inputField.style.fontFamily = "monospace"
+    inputField.addEventListener("input", (e) => {
+        const inputField = document.getElementById("input")
+        guess = inputField.value
+    })
+
+    parentNode.appendChild(inputField)
+
+    return inputField
+}
+
+const addCheckButton = (parentNode) => {
+    // Button to check the input
+    const checkButton = document.createElement("button")
+    checkButton.id = "checkButton"
+    checkButton.innerHTML = "Check"
+    checkButton.addEventListener("click", () => {
+        if (isValidInput(guess)) {
+            validInput()
+            addTry(guess)
+        } else {
+            unvalidInput()
+        }
+    })
+
+    parentNode.appendChild(checkButton)
+
+    return addCheckButton
+}
+
+const addErrMsg = (parentNode) => {
+    // Error message if needed
+    const errMsg = document.createElement("div")
+    errMsg.id = "err"
+    errMsg.style.color = "red"
+    errMsg.style.fontSize = "0.5em"
+    errMsg.style.textAlign = "left"
+    errMsg.innerText = ""
+
+    app.appendChild(errMsg)
+
+    return errMsg
+}
+
 const main = (
     () => {
         const app = document.getElementById("app")
-        let guess = ""
 
-        // Text box to input sha256
-        const input = document.createElement("input")
-        input.type = "text"
-        input.id = "input"
-        input.maxLength = 64
-        input.autofocus = "autofocus"
-        input.size = 64
-        input.style.fontFamily = "monospace"
-        input.addEventListener("input", e => {
-            const input = document.getElementById("input")
-            guess = input.value
-        })
-        app.appendChild(input)
-
-        // Button to check the input
-        const checkButton = document.createElement("button")
-        checkButton.innerHTML = "Check"
-        checkButton.addEventListener("click", _ => {
-            console.log(guess)
-        })
-        app.appendChild(checkButton)
+        const inputField = addInputField(app)
+        addCheckButton(app)
+        addErrMsg(app)
     }
 )
 
