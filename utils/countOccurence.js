722c58dd3d35aff88ab78b4de9170df5d2fb495a
Keftcha <alban.dewilde@gmail.com>
2022-02-26 22:00:29 +0100

    Use a more descriptive variable name

--- utils/countOccurence.js
+++ utils/countOccurence.js
@@ -4,10 +4,10 @@ const countOccurence = (word, character) => {
     return word.split("").filter(c => c === character).length
 }
 
-const countOccurenceGoodPlaceAfter = (i, guess, secret, character) => {
+const countOccurenceGoodPlaceAfter = (start, guess, secret, character) => {
     let nb = 0
 
-    for (; i < secret.length; i++) {
+    for (let i = start; i < secret.length; i++) {
         if (guess[i] === character && secret[i] === character) {
             nb++
         }
